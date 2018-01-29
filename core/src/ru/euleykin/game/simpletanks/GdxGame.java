bfa7a6ba05b6db19c1813eee0f069197ba505bfe
Evgeny Uleykin <euleykin@getmobit.ru>
2018-01-29 14:46:54 +0300

    implement fire

--- core/src/ru/euleykin/game/simpletanks/GdxGame.java
+++ core/src/ru/euleykin/game/simpletanks/GdxGame.java
@@ -10,8 +10,21 @@ import ru.euleykin.game.simpletanks.model.Bullet;
 
 public class GdxGame extends ApplicationAdapter {
 
+    /**
+     * Хотел сделать красиво, но мне не нравится мой код.
+     * Много часов потратил на то, чтобы найти как повернуть текстуру.
+     * Не нашел, узнал про sprite, который создается из текстуры. Сделал через него.
+     * Сильно не уверен что делаю правильно, наверное из-за недопонимания.
+     * У Sprite нет вектора, есть просто координаты x, y. Поэтому не использовал какие-либо операции с векторами,
+     * что наталкивает меня на мысль что я как-то не так решил задачу, но не знаю как ее по-другому можно решить.
+     *
+     *
+     */
+
     public static final int WINDOW_WIDTH = 1024;
     public static final int WINDOW_HEIGHT = 768;
+
+    private static final float DEF_POWER = 2.0f;
 
 	private SpriteBatch batch;
 	private Grass grass;
@@ -20,11 +33,10 @@ public class GdxGame extends ApplicationAdapter {
 
 	private float shotPower;
 	private boolean shotInProgress;
-	private float time = 0;
+//	private float time = 0;
 	
 	@Override
 	public void create () {
-        shotInProgress = false;
 		batch = new SpriteBatch();
         grass = new Grass();
         initPlayerTank();
@@ -34,6 +46,18 @@ public class GdxGame extends ApplicationAdapter {
                 playerTank.getTurretSprite().getY(),
                 100, 100);
         bullet.getSprite().setOrigin(0-playerTank.getTurretSprite().getWidth(), 0);
+        shotPower = DEF_POWER;
+        shotInProgress = false;
+        initBullet();
+    }
+
+    private void initBullet() {
+        bullet.getSprite().setPosition(
+                playerTank.getTurretSprite().getX() + playerTank.getTurretSprite().getWidth(),
+                playerTank.getTurretSprite().getY());
+        bullet.getSprite().setRotation(playerTank.getTurretSprite().getRotation());
+        bullet.getVelocity().x = 100;
+        bullet.getVelocity().y = 100;
     }
 
 	private void initPlayerTank() {
@@ -51,7 +75,8 @@ public class GdxGame extends ApplicationAdapter {
 		batch.begin();
         drawGrass();
         playerTank.render(batch);
-        bullet.render(batch);
+        if (shotInProgress)
+            bullet.render(batch);
 		batch.end();
 	}
 
@@ -69,35 +94,49 @@ public class GdxGame extends ApplicationAdapter {
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            shotPower+=dt;
+            shotPower-=dt;
+            initBullet();
         }
-        else if (!shotInProgress ) {
+        else if (shotPower < DEF_POWER ) {
             shotInProgress = true;
-            time = 0;
+//            time = 0;
 //            float posX = (float) Math.cos(Math.toRadians(bullet.getSprite().getRotation()) * bullet.getSprite().getWidth() * bullet.getSprite().getX());
 //            float posY = (float) Math.sin(Math.toRadians(bullet.getSprite().getRotation()) * bullet.getSprite().getHeight() * bullet.getSprite().getY());
 //            bullet.getSprite().setPosition(posX, posY);
         }
-        if (shotInProgress)
-            fire(dt);
+        if (shotInProgress) {
+//            fire(dt);
+            System.out.println("FIRE");
+            bullet.getVelocity().y -= shotPower;
+            bullet.getSprite().setPosition(bullet.getSprite().getX() + bullet.getVelocity().x * dt,
+                    bullet.getSprite().getY() + bullet.getVelocity().y * dt);
+            float angle = bullet.getVelocity().angle();
+            bullet.getSprite().setRotation(angle);
+
+        }
+        if (bullet.getSprite().getY() - bullet.getTexture().getHeight()  < grass.getTexture().getHeight()) {
+            shotPower = DEF_POWER;
+            shotInProgress = false;
+            bullet.getSprite().setPosition(0 - bullet.getTexture().getWidth(), 0 - bullet.getTexture().getHeight());
+        }
 	}
 
-    private void fire(float dt) {
-	    if (shotPower < 1.0f) {
-	        shotInProgress = false;
-	        shotPower = 0.0f;
-	        return;
-        }
-        shotPower = MathUtils.clamp(shotPower, 1.0f, 2.0f);
-	    time += dt;
-	    float posX = shotPower * 100 * time * (float) Math.cos(Math.toRadians(bullet.getSprite().getRotation()));
-        float posY = shotPower * 100 * time * (float) Math.sin(Math.toRadians(bullet.getSprite().getRotation())) - 9.8f * 100 * (float) Math.pow(time, 2) * 0.5f;
-        bullet.getSprite().setPosition(posX, posY);
-        if (bullet.getSprite().getY() < grass.getTexture().getHeight()) {
-            shotPower = 0;
-            shotInProgress = false;
-        }
-    }
+//    private void fire(float dt) {
+//	    if (shotPower < 1.0f) {
+//	        shotInProgress = false;
+//	        shotPower = 0.0f;
+//	        return;
+//        }
+//        shotPower = MathUtils.clamp(shotPower, 1.0f, 2.0f);
+//	    time += dt;
+//	    float posX = shotPower * 100 * time * (float) Math.cos(Math.toRadians(bullet.getSprite().getRotation()));
+//        float posY = shotPower * 100 * time * (float) Math.sin(Math.toRadians(bullet.getSprite().getRotation())) - 9.8f * 100 * (float) Math.pow(time, 2) * 0.5f;
+//        bullet.getSprite().setPosition(posX, posY);
+//        if (bullet.getSprite().getY() < grass.getTexture().getHeight()) {
+//            shotPower = 0;
+//            shotInProgress = false;
+//        }
+//    }
 
     private void drawGrass() {
 	    int count = WINDOW_WIDTH/grass.getTexture().getWidth();
